4c825dbef6c7700601018207bf667db3a47e38ee
GaLiLeo200 <tasneemreza02@gmail.com>
2026-05-20 20:45:43 +0530

    Added Patterns

---
+++ Pattern/Hollowbutterfly.java
@@ -0,0 +1,7 @@
+public class Hollowbutterfly{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+    }
+}
