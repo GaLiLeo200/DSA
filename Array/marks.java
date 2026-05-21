9bdc7964b59c23c04593d4cb3c0145c32a0eb086
GaLiLeo200 <tasneemreza02@gmail.com>
2026-05-21 10:34:21 +0530

    Added array

---
+++ Array/marks.java
@@ -0,0 +1,12 @@
+public class marks {
+    public static void main(String[] args){
+        int[] marks = new int[3];
+        marks[0] = 100;
+        marks[1] = 90;
+        marks[2] = 80;
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
