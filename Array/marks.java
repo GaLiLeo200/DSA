57f7d4235510660998080d43358db5238673089c
GaLiLeo200 <tasneemreza02@gmail.com>
2026-05-21 10:47:58 +0530

    Updated the marks in a array

--- Array/marks.java
+++ Array/marks.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 public class marks {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the marks of 3 subjects:");
-        int[] marks = new int[3];
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        System.out.println("Enter the marks of " + n + " subjects:");
+
+        int[] marks = new int[n];
         for(int i=0; i<marks.length; i++){
             marks[i] = sc.nextInt();
         }
         for(int i=0; i<marks.length; i++){
-            System.out.println(marks[i]);
+            System.out.println("Your marks[" + i + "]: " + marks[i]);
         }
     }
     
