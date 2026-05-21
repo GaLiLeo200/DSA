60d7e277136aab5071737384e2da2887d04d283a
GaLiLeo200 <tasneemreza02@gmail.com>
2026-05-21 10:38:56 +0530

    Updated the marks with loops input

--- Array/marks.java
+++ Array/marks.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 public class marks {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the marks of 3 subjects:");
         int[] marks = new int[3];
-        marks[0] = 100;
-        marks[1] = 90;
-        marks[2] = 80;
+        for(int i=0; i<marks.length; i++){
+            marks[i] = sc.nextInt();
+        }
         for(int i=0; i<marks.length; i++){
             System.out.println(marks[i]);
         }
